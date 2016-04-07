3494e0d79afa61b9f66b8e15313f531e520ba3ef
Jesse Smith <jesse.etan.smith@gmail.com>
2016-04-07 09:22:25 -0400

    error handling in converting to spreadsheet

--- main.py
+++ main.py
@@ -53,7 +53,10 @@ for fn in os.listdir(myPath + '/sequences'):
                        temp = line.split(',')[1]
                        temp = temp[:temp.find('\\') - 1]
                        if temp != '' and ('-2' not in temp) and temp != '-1' and 'FAL' not in temp:
-                           spreadSheetArray.append([float(latitude), float(longitude), float(temp)])
+                            try:
+                                spreadSheetArray.append([float(latitude.strip("\"")), float(longitude.strip("\"")), float(temp.strip("\""))])
+                            except:
+                                print("error converting to float")
                        continue
                    elif col == 7:
                        longitude = word
@@ -67,15 +70,18 @@ for fn in os.listdir(myPath + '/sequences'):
                if testCount == -10:
                    break
        if(spreadSheetArray != []):
-           masterArray.extend([spreadSheetArray[i:i+10] for i in xrange(0, len(spreadSheetArray), 10)])
+           #masterArray.append(spreadSheetArray)
+           masterArray.extend([spreadSheetArray[i:i+11] for i in xrange(0, len(spreadSheetArray), 11)])
        if (spreadSheetArrayTime != []):
            masterArrayTime.append(spreadSheetArrayTime)
 
 seqs = masterArray
 steps = map(lambda l: l[len(l) - 1][:2], seqs)
+for l in seqs:
+    del l[-1]
 timeseqs = masterArrayTime
 modeltimeseq = max(enumerate(masterArrayTime), key = lambda tup: len(tup[1]))[1]
-
+print modeltimeseq
 
 ########################################################################################################################
 
@@ -171,7 +177,6 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Masking, Dropout
 from keras.layers.recurrent import LSTM
 from keras.optimizers import SGD
-from keras.utils.visualize_util import plot, to_graph
 
 #build and train model
 in_dimension = 3
@@ -181,13 +186,12 @@ out_dimension = 2
 model = Sequential()
 model.add(Masking([0,0,0], input_shape=(max_sequence_length, in_dimension)))
 model.add(LSTM(hidden_neurons, activation='softmax', return_sequences=False, input_shape=(max_sequence_length, in_dimension)))
-model.add(Dropout(0.2))
-model.add(Dropout(0.2))
+model.add(Dropout(0.5))
 model.add(Dense(out_dimension, activation='linear'))
 
-sgd = SGD(lr=0.05, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss="mse", optimizer=sgd)
-model.fit(padded_training_seqs, training_final_steps, nb_epoch=10, batch_size=32, verbose=2, show_accuracy=True)
+model.fit(padded_training_seqs, training_final_steps, nb_epoch=10, batch_size=32, verbose=2)
 
 #fetch temperature from NOAA and inject into location tuple
 def loc_with_temp(ll, i):
@@ -195,21 +199,15 @@ def loc_with_temp(ll, i):
     return [ll[0], ll[1], temp]
 
 #generate new sequence based on seed
-seed_lat = 44.9
-seed_long = 37.7
-seed_temp = 9.5
+seed = [44.80696869, 38.74343872, 9.243]
 
-current_generated_sequence = np.array([[[seed_lat, seed_long, seed_temp]] + [[0,0,0]] * (max_sequence_length - 1)], dtype=np.dtype(float))
+gen_seq_len = 1000
 
-for i in range(0, max_sequence_length - 1):
-    next_step = model.predict(current_generated_sequence, batch_size=10, verbose=2)[0]
+current_generated_sequence = np.array([[[seed[0], seed[1], seed[2]]] + [[0,0,0]] * (gen_seq_len - 1)], dtype=np.dtype(float))
+for i in range(0, gen_seq_len - 1):
+    next_step = model.predict(current_generated_sequence, batch_size=10, verbose=1)[0]
     current_generated_sequence[0][i + 1] = loc_with_temp(next_step, i)
-    #print new iteration
-    print(current_generated_sequence[0][i])
+    print(current_generated_sequence)
 
 np.set_printoptions(threshold=np.nan)
 print(current_generated_sequence)
-
-
-graph = to_graph(model, show_shape=True)
-graph.write_png("model.png")
