e09772af990fedb8a2304957f5544adeef8c874f
Jesse Smith <jesse.etan.smith@gmail.com>
2016-04-12 18:09:28 -0400

    cleaned up old seqs and removed repetition code

--- main.py
+++ main.py
@@ -199,28 +199,14 @@ def loc_with_temp(ll, i):
     return [ll[0], ll[1], temp]
 
 #generate new sequence based on seed
-newest_seed = [44.80696869, 38.74343872, 9.243]
-
+seed = [44.80696869, 38.74343872, 9.243]
 gen_seq_len = 50
-main_seq = []
+current_generated_sequence = np.array([[[seed[0], seed[1], seed[2]]] + [[0,0,0]] * (gen_seq_len - 1)], dtype=np.dtype(float))
 
-def make_seq(seed, iteration):
-    current_generated_sequence = np.array([[[seed[0], seed[1], seed[2]]] + [[0,0,0]] * (gen_seq_len - 1)], dtype=np.dtype(float))
-    for i in range(0, gen_seq_len - 1):
-        next_step = model.predict(current_generated_sequence, batch_size=10, verbose=1)[0]
-        current_generated_sequence[0][i + 1] = loc_with_temp(next_step, i + gen_seq_len * iteration)
-        main_seq.append(current_generated_sequence[0][i + 1])
-        print main_seq
-    return current_generated_sequence
-
-for i in range(0, 5):
-    newest_seq = make_seq(newest_seed, i)
-    newest_seed = newest_seq[0][0]
-
-import itertools
-def grouper(n, iterable, fillvalue=None):
-    args = [iter(iterable)] * n
-    return itertools.zip_longest(*args, fillvalue=fillvalue)
-
+for i in range(0, gen_seq_len - 1):
+   next_step = model.predict(current_generated_sequence, batch_size=1, verbose=1)[0]
+   print(next_step)
+   current_generated_sequence[0][i + 1] = loc_with_temp(next_step, i)
+   
 np.set_printoptions(threshold=np.nan)
-print(main_seq)
+print(current_generated_sequence)
